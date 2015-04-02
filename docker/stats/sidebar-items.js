786c9f3db00b4a7eb20f84b45c36f9020786f25f
Graham Lee <ghmlee@ghmlee.com>
2015-04-02 17:09:52 +0900

    initial commit

---
+++ docker/stats/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Stats",""],["blkio_stat",""],["blkio_stats",""],["cpu_stats",""],["cpu_usage",""],["memory_stats",""],["network",""],["stats",""],["throttling_data",""]]});
---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['docker'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='docker/container/struct.Port.html' title='docker::container::Port'>Port</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
