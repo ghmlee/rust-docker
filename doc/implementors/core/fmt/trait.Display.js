6f682b334040014f4ad6129922e15c9c301a317a
Graham Lee <ghmlee@ghmlee.com>
2015-04-23 09:00:22 +0000

    It is bulit at c48c11b6f7de69f248bc07961538a557e50ab1a4.

--- doc/implementors/core/fmt/trait.Display.js
+++ doc/implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['docker'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='docker/container/struct.Container.html' title='docker::container::Container'>Container</a>",];
+implementors['docker'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='docker/container/struct.Container.html' title='docker::container::Container'>Container</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
